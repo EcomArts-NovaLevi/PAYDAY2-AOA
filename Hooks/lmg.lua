e5b2fb39e9c930fa40af09455b49ccf36a4e551b
Nova Levi <136199398+EcomArts-NovaLevi@users.noreply.github.com>
2023-06-22 20:24:31 -0500

    Add files via upload
    LMG rebalances, mostly commenting out spread stat

--- Hooks/lmg.lua
+++ Hooks/lmg.lua
@@ -6,7 +6,7 @@ function WeaponTweakData:init(tweak_data)
 --LIGHT MACHINE GUNS
 
 --Hk Hk21
-self.hk21.stats.spread = 80
+--self.hk21.stats.spread = 80
 self.hk21.stats.recoil = 70
 self.hk21.stats.reload = 20
 self.hk21.AMMO_MAX = 340
@@ -14,12 +14,13 @@ self.hk21.AMMO_MAX = 340
 --Hk HK51B
 
 self.hk51b.AMMO_MAX = 300
-self.hk51b.stats.spread = 70
-self.hk51b.stats.recoil = 60
+--self.hk51b.stats.spread = 70
+--self.hk51b.stats.recoil = 60
+self.hk51b.stats.reload = 18
 
 --KAC ChainSAW
 self.kacchainsaw.AMMO_MAX = 420
-self.kacchainsaw.stats.spread = 70
+--self.kacchainsaw.stats.spread = 70
 self.kacchainsaw.stats.recoil = 60
 self.kacchainsaw.stats.reload = 15
 self.kacchainsaw.stats.damage = 98
@@ -27,18 +28,18 @@ self.kacchainsaw.stats.damage = 98
 --KSP 58
 self.par.AMMO_MAX = 600
 self.par.stats.damage = 108
-self.par.stats.spread = 80
+--self.par.stats.spread = 80
 self.m249.stats.recoil = 70
 self.par.stats.reload = 19
 
 --M249 SAW
-self.m249.stats.spread = 80
+--self.m249.stats.spread = 80
 self.m249.stats.recoil = 70
-self.m249.stats.reload = 20
+self.m249.stats.reload = 17
 self.m249.stats.damage = 105
 
 --M60
-self.m60.stats.spread = 80
+--self.m60.stats.spread = 80
 self.m60.stats.recoil = 70
 self.m60.stats.reload = 20
 self.m60.AMMO_MAX = 550
@@ -55,7 +56,7 @@ self.mg42.stats.damage = 90
 --self.hcar.stats.recoil = 60
 
 --RPK
-self.rpk.stats.spread = 80
+--self.rpk.stats.spread = 80
 self.rpk.stats.recoil = 70
 self.rpk.stats.reload = 20
 end
