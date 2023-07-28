6904ebd76d0edd50257a75a2cb70298aed04e849
Nova Levi <136199398+EcomArts-NovaLevi@users.noreply.github.com>
2023-07-28 13:26:19 -0500

    Add files via upload
    Occurred to me that I have no idea what I'm doing when it comes to snipers.  Now I think I do (big sniper tweaks).

--- Hooks/sniper.lua
+++ Hooks/sniper.lua
@@ -6,86 +6,100 @@ function WeaponTweakData:init(tweak_data)
 --SNIPER RIFLES
 
 --AI AT308
-self.awp.AMMO_MAX = 42
+self.awp.AMMO_MAX = 30
 --self.awp.stats.reload = 20
 self.awp.AMMO_PICKUP = {4, 10}
 
 --Barrett M95
-self.m95.AMMO_MAX = 30
+self.m95.AMMO_MAX = 25
 --self.m95.stats.reload = 20
 self.m95.AMMO_PICKUP = {4, 10}
 self.m95.fire_mode_data.fire_rate = 0.155
 
+--Blaser R93
+self.r93.AMMO_MAX = 45
+self.r93.AMMO_PICKUP = {4, 10}
+
 --Desert Fox SRS-1
 --self.desertfox.stats.reload = 15
-self.desertfox.AMMO_MAX = 60
-self.desertfox.stats.concealment = 30
-self.desertfox.fire_mode_data.fire_rate = 0.3
+self.desertfox.AMMO_MAX = 35
+--self.desertfox.stats.concealment = 30
+--self.desertfox.fire_mode_data.fire_rate = 0.3
 
 --M91 PU Mosin Nagant
 self.mosin.AMMO_MAX = 65
 --self.mosin.stats.reload = 20
-self.mosin.AMMO_PICKUP = {15.00, 30.00}
+self.mosin.AMMO_PICKUP = {4, 10}
 
 --Marlin 1895 SBL
 --self.sbl.stats.reload = 20
 self.sbl.stats.damage = 390
-self.sbl.AMMO_PICKUP = {10.00, 15.00}
+self.sbl.AMMO_PICKUP = {4, 10}
 
 --QBU-88
 self.qbu88.AMMO_MAX = 140
 --self.qbu88.stats.reload = 20
 self.qbu88.stats.damage = 300
 self.qbu88.fire_mode_data.fire_rate = 0.2
+self.qbu88.AMMO_PICKUP = {4, 10}
 
 --Remington MSR
 self.msr.AMMO_MAX = 80
+self.msr.stats.damage = 300
 --self.msr.stats.reload = 20
-self.msr.stats.concealment = 26
+--self.msr.stats.concealment = 26
+self.msr.AMMO_PICKUP = {4, 10}
 
 --Remington Model 700
-self.r700.stats.damage = 350
+self.r700.stats.damage = 320
+self.r700.AMMO_PICKUP = {4, 10}
 
 --Springfield SAINT Victor AR-15
 --self.victor.stats.reload = 20
-self.victor.stats.damage = 300
+self.victor.stats.damage = 240
 self.victor.AMMO_MAX = 80
+self.victor.AMMO_PICKUP = {4, 10}
 
 --Steyr Scout
 --self.scout.stats.reload = 20
-self.scout.stats.damage = 300
+self.scout.stats.damage = 270
 self.scout.AMMO_MAX = 45
+self.scout.AMMO_PICKUP = {4, 10}
 
 --SVD Dragunov
 self.siltstone.AMMO_MAX = 60
+--self.siltstone.stats.damage = 250
 --self.siltstone.stats.concealment = 27
 --self.siltstone.stats.reload = 20
-self.siltstone.AMMO_PICKUP = {4.22, 4.86}
+self.siltstone.AMMO_PICKUP = {4, 10}
 
 --Thompson G2 Contender
 --self.contender.stats.reload = 20
-self.contender.stats.damage = 300
+--self.contender.stats.damage = 300
 self.contender.AMMO_MAX = 80
+self.contender.AMMO_PICKUP = {4, 10}
 
 --TTI 308
 self.tti.AMMO_MAX = 140
 --self.tti.stats.reload = 20
-self.tti.stats.damage = 300
+self.tti.stats.damage = 340
 self.tti.fire_mode_data.fire_rate = 0.2
+self.tti.AMMO_PICKUP = {4, 10}
 
 --Walther WA2000 Model 1
 self.wa2000.AMMO_MAX = 80
 --self.wa2000.stats.reload = 20
 --self.wa2000.stats.concealment = 26
+self.wa2000.AMMO_PICKUP = {4, 10}
 
 --Winchester 1874
 --self.winchester1874.stats.reload = 20
 self.winchester1874.stats.damage = 390
-self.winchester1874.AMMO_PICKUP = {10.00, 15.00}
+self.winchester1874.AMMO_PICKUP = {4, 10}
 
 --Winchester Model 70
 self.model70.AMMO_MAX = 80
 --self.model70.stats.concealment = 28
 --self.model70.stats.reload = 20
-self.model70.AMMO_PICKUP = {10.00, 15.00}
+self.model70.AMMO_PICKUP = {4, 10}
 end
