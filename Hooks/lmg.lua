a1f394d31b56384edf2eabce7eaf6577b2d0bf77
Nova Levi <136199398+EcomArts-NovaLevi@users.noreply.github.com>
2026-01-29 21:30:14 -0600

    LMGs
    I tried to balance the Campbell so much but it just fell somewhere kind of next to where it was before ig

--- Hooks/lmg.lua
+++ Hooks/lmg.lua
@@ -15,13 +15,13 @@ self.hk21.AMMO_MAX = 340
 
 self.hk51b.AMMO_MAX = 300
 --self.hk51b.stats.spread = 70
---self.hk51b.stats.recoil = 60
+self.hk51b.stats.recoil = 60
 self.hk51b.stats.reload = 18
 
 --KAC ChainSAW
 self.kacchainsaw.AMMO_MAX = 420
---self.kacchainsaw.stats.spread = 70
---self.kacchainsaw.stats.recoil = 60
+--self.kacchainsaw.stats.spread = 30
+--self.kacchainsaw.stats.recoil = 24
 self.kacchainsaw.stats.reload = 15
 self.kacchainsaw.stats.damage = 98
 
@@ -33,6 +33,7 @@ self.par.stats.damage = 107
 self.par.stats.reload = 19
 
 --M249 SAW
+self.m249.AMMO_MAX = 300
 --self.m249.stats.spread = 80
 --self.m249.stats.recoil = 70
 self.m249.stats.reload = 17
@@ -51,7 +52,7 @@ self.mg42.stats.reload = 17
 self.mg42.stats.damage = 85
 
 --Ohio Ordinance HCAR
---self.hcar.AMMO_MAX = 180
+self.hcar.AMMO_MAX = 180
 --self.hcar.stats.spread = 75
 --self.hcar.stats.recoil = 60
 
